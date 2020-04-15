1fdd5697af752af2a39c1a703de2fadc307454dd
divoskov <62335678+divoskov@users.noreply.github.com>
2020-04-15 18:25:50 +0300

    Add files via upload

---
+++ 1342. Number of Steps to Reduce a Number to Zero.cs
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+public class Solution {
+    public int NumberOfSteps (int num) {
+        int res = 0;
+        while (num != 0){
+            if (num % 2 == 0)
+                num /= 2;
+            else
+                num -= 1;
+            res++;
+        }
+        return res;
+    }
+}
